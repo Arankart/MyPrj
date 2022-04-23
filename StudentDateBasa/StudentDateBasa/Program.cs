50af8c9d7f5b82b30402d44a8079e918bd5061eb
Daniil Afanasyev <35265417+Arankart@users.noreply.github.com>
2022-04-23 14:49:32 +0300

    3. База данных участников:

---
+++ StudentDateBasa/StudentDateBasa/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StudentDateBasa
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Members> mem = new List<Members>();
+
+            for (int i = 0; i < 3; i++) mem.Add(new Members() { name = Console.ReadLine(), age = Convert.ToInt32(Console.ReadLine()) });
+
+            Console.WriteLine("———————————————————");
+            Console.WriteLine("");
+            Console.WriteLine("Output:");
+
+
+
+            foreach (var people in mem)
+            {
+                Console.WriteLine(people.name);
+                Console.WriteLine(people.age);
+                Console.WriteLine("");
+            }
+
+            Console.ReadKey();
+        }
+    }
+
+    public class Members
+    {
+        public string name;
+        public int age;
+    }
+}
